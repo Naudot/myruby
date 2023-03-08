df4f451f89de9720572c9eefa540135beb76b6c2
nnaudot <nnaudot@manzalab.com>
2023-03-08 11:00:32 +0100

    Update the chess code and try using debug

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,12 +1,17 @@
 class PlayersController < ApplicationController
 
   def index
-	@players = Player.all
+	
+	# View variables
+	@players = Player.all # Get all players from Database
+	@championSuitorsHash = Hash.new # Key is ELO, Value is an array of Players
+	@champions = [] # Final list of champions
 
-	# Key is ELO, Value is an array of Players
-	@championSuitorsHash = Hash.new
+	if @players == nil
+		return
+	end
 
-	# First step, we sort players in a hash using their ELO and age.
+	# First step : We put players in a hash using their ELO and comparing their age.
 
 	# For each player, we check the players with the same elo
 	@players.each do |player|
@@ -14,28 +19,28 @@ class PlayersController < ApplicationController
 
 		if championSuitors == nil # If there is no one at this ELO yet
 			@championSuitorsHash[player.elo] = [player] # We create the array with the player
-		else
-			championSuitor = championSuitors[0] # Because every champions at this ELO is same ELO and age, we can only check the first of the champion suitors
-			if championSuitor.age < player.age # Check if the player is younger than the champion(s)
+		else # Else if there are already one or more players with this ELO 
+			championSuitor = championSuitors[0] # Because every champions at this ELO is same ELO and age, we can check only the first of the champion suitors
+			
+			if championSuitor.age < player.age # Check if the player is younger than the champion suitor(s) and if this is the case, the player replaces the champion suitor(s)
 				@championSuitorsHash[player.elo].clear()
 				@championSuitorsHash[player.elo] = [player]
-			elsif championSuitor.age == player.age # If same age, we add the player as a potential champion
+			elsif championSuitor.age == player.age # If same age, we add the player as a potential champion suitor
 				@championSuitorsHash[player.elo].push(player)
 			end
 		end
 	end
 	
-	currentYoungest = Date.parse('01-01-0001')
-
+	# We sort the hash by ELO from highest to lowest
 	@championSuitorsHash = @championSuitorsHash.sort.reverse.to_h
-	@champions = []
 
-	# Second step, we check if they are champions
+	# Second step, we check if they are champions by going through every encounterd ELOs
 
+	currentYoungest = Date.parse('01-01-0001')
 	# We save the age of the maximum ELO champion then we compare it when going down in ELO
 	@championSuitorsHash.keys.each do |championElo|
 		# Because we are going down on ELOs, we only need to check if the best player at the current ELO is younger than the previous champion
-		if @championSuitorsHash[championElo][0].age > currentYoungest
+		if @championSuitorsHash[championElo][0].age > currentYoungest # We check if his birth date is more recent than the previous champion birth date
 			currentYoungest = @championSuitorsHash[championElo][0].age
 			championsAtSameELO = []
 			@championSuitorsHash[championElo].each do |champion|
@@ -49,19 +54,5 @@ class PlayersController < ApplicationController
 
   def show
 	@player = Player.find(params[:id])
-
-	# petite idée pour l'exo du chess, on créé un hashset des elos, et à chaque elo on link le joueur à cet élo qui a le plus bas âge
-	# donc on a juste besoin de parcourir chaque joueur et leur élo une fois
-	# ensuite on parcours tous les élos en partant de l'élo le plus élevé (la personne avec l'élo le plus élevé sera toujours championne)
-	# en gardant l'âge le plus bas et on détermine les@championSuitorsHash
-	# exemple : ELO 3000 John âge 25 -> Champion
-	# ELO 2999 Jean âge 30 -> PAS Champion car âge mini 25
-	# ELO 2998 Marie âge 25 -> Championne car âge mini 25, l'âge mini passe à 24
-	# 
-	# l'âge mini est de 25 quand on dépasse John et l'élo max est 3000 donc c'est un champion
-	# mais l'âge mini au dessus de Marie est 30 ce qui est inférieur donc ce n'est pas une championne
-	# je considère deux personnes nées le même jour et au même élo toutes les deux championnes
-
-	# donc la structure de données finale est un tableau d'élo, chaque élo associé à une ou plusieurs personne et à une date de naissance
   end
 end
